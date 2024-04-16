7f48dce02086e01ea48976b59295d222bb4f6a29
DaniCarmo <dani93elle@hotmail.com>
2024-04-16 10:29:22 +0100

    create urls, css and html files for checkout main page and app

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import (render, redirect, reverse, get_object_or_404, HttpResponse)
+from django.views.decorators.http import require_POST
+from django.contrib import messages
+from django.conf import settings
 
-# Create your views here.
+from .forms import OrderForm
+from .models import Order, OrderLineItem
+
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "Oops, looks like there's nothing in your bag")
+        return redirect(reverse('products'))
+    
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
