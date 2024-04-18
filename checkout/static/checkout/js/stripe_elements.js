8bd9d497a6684c3995976b11cbc9fb4cd8345ced
DaniCarmo <dani93elle@hotmail.com>
2024-04-18 10:55:15 +0100

    update strip functionalisty in checkout views and settings

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -19,3 +19,19 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+//Handle realtime validation errors on cards
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementByID('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
