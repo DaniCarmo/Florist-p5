684162fe05305a96aebb4dbe11931dc5de884f51
DaniCarmo <dani93elle@hotmail.com>
2024-05-15 12:33:17 +0100

    move media folder into static folder and update file paths

--- florist/settings.py
+++ florist/settings.py
@@ -170,8 +170,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/' - delete
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media') - delete
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
