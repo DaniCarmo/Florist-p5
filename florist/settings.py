6f068cc53b34f09f4ae92fb3e84ec521343a7b96
DaniCarmo <dani93elle@hotmail.com>
2024-05-30 10:35:20 +0100

    update requirements and settings.py with dj database and cloudinary configs

--- florist/settings.py
+++ florist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-%07@+nuf%ghx=7m%(od@o-41u0b@(xjb26k6n^fh&h5ikygmqr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -123,12 +124,28 @@ WSGI_APPLICATION = 'florist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+# If running on Heroku
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
-}
+else:
+    # If running locally or in non-Heroku environment
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
@@ -184,6 +201,10 @@ DEFAULT_FROM_EMAIL = 'happyflorist@gmail.com'
 
 #STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 #STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
+# Cloudinary settings
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+CLOUIDNARY_URL = os.environ.get('CLOUDINARY_URL')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
