97d34679f3da0cfe38d923332ac1274c44917382
DaniCarmo <dani93elle@hotmail.com>
2024-06-10 16:34:28 +0100

    update event forms.py to fix add event error

--- events/forms.py
+++ events/forms.py
@@ -3,6 +3,13 @@ from .models import Event
 
 
 class EventForm(forms.ModelForm):
+
     class Meta:
         model = Event
         fields = ["title", "description", "date", "time", "location"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
