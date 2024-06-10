db68ae1e21146b9d49dfc405e026e054f69eaa1b
DaniCarmo <dani93elle@hotmail.com>
2024-06-10 11:21:24 +0100

    add robots.txt file and update fb page link on footer

--- florist/urls.py
+++ florist/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +28,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('events/', include('events.urls')),
+    re_path(r'^robots\.txt$', serve, {'path': 'robots.txt', 'document_root': settings.BASE_DIR, 'show_indexes': False}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
