261ef8edd0878008db4ca4750bb7a2d7a5c99b3b
DaniCarmo <dani93elle@hotmail.com>
2024-06-13 11:25:34 +0100

    update wishlist forms anbd views to fix error with wishlist page rendering

--- wishlist/forms.py
+++ wishlist/forms.py
@@ -6,5 +6,3 @@ class WishlistForm(forms.ModelForm):
     class Meta:
         model = Wishlist
         fields = ['product', 'quantity']
-    
-    product = forms.ModelChoiceField(queryset=Product.objects.all(), label="Product")
--- wishlist/views.py
+++ wishlist/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Wishlist
 from .forms import WishlistForm
+from products.models import Product
 
 @login_required
 def wishlist_list(request):
@@ -9,16 +10,20 @@ def wishlist_list(request):
     return render(request, 'wishlist_list.html', {'wishlists': wishlists})
 
 @login_required
-def wishlist_add(request):
+def wishlist_add(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+
     if request.method == 'POST':
         form = WishlistForm(request.POST)
         if form.is_valid():
             wishlist_item = form.save(commit=False)
             wishlist_item.user = request.user
+            wishlist_item.product = product
             wishlist_item.save()
             return redirect('wishlist_list')
     else:
         form = WishlistForm()
+
     return render(request, 'wishlist_form.html', {'form': form})
 
 @login_required
