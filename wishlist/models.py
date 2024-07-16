a1d41c587c0234b9f47502a525b7d53ebd6de17b
DaniCarmo <dani93elle@hotmail.com>
2024-07-16 16:05:49 +0100

    update wishlist model to include product sizes

--- wishlist/models.py
+++ wishlist/models.py
@@ -6,6 +6,7 @@ from products.models import Product
 class Wishlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    size = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.user.username}'s wishlist item: {self.product.name}"
+        return self.id
