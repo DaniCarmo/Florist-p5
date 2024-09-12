cdbda9478076e38e03d1ec73c2f4099971282f01
DaniCarmo <dani93elle@hotmail.com>
2024-09-12 13:00:24 +0100

    set debug to true for troublshooting

--- florist/settings.py
+++ florist/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['the-happy-florist-5c44ce792179.herokuapp.com',
                  'localhost', '127.0.0.1']
