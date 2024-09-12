6b998d34273e194d380ef948dccf859331bf2897
DaniCarmo <dani93elle@hotmail.com>
2024-09-12 09:35:09 +0100

    fix syntax issue caused by making lines shorter

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -111,9 +111,12 @@ class StripeWH_Handler:
         if order_exists:
             self._send_confirmation_email(order)
             return HttpResponse(
-                        content=f'Webhook received: {event["type"]} |
-                        SUCCESS: Verified order already in database',
-                        status=200)
+                content=(
+                    f'Webhook received: {event["type"]} | '
+                    'SUCCESS: Verified order already in database'
+                ),
+                status=200
+            )
         else:
             order = None
             try:
@@ -154,13 +157,20 @@ class StripeWH_Handler:
                 if order:
                     order.delete()
                 return HttpResponse(
-                    content=f'Webhook received: {event["type"]} | ERROR: {e}',
-                    status=500)
+                    content=(
+                        f'Webhook received: {event["type"]} | '
+                        'SUCCESS: Created order in webhook'
+                    ),
+                    status=200
+                )
         self._send_confirmation_email(order)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]} |
-            SUCCESS: Created order in webhook',
-            status=200)
+            content=(
+                f'Webhook received: {event["type"]} | '
+                'SUCCESS: Created order in webhook'
+            ),
+            status=200
+        )
 
     def handle_payment_intent_payment_failed(self, event):
         """
